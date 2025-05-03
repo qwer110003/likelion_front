c041dc9ae6a9322a7c9b46e9bf8c6edb1fd8b66a
jeong-yejun <jeong-yejun@localhost>
2025-05-03 21:28:01 +0900

    hyy

--- likelion_front-2/src/main/java/com/example/likelon_front_2/controller/HomeController.java
+++ likelion_front-2/src/main/java/com/example/likelon_front_2/controller/HomeController.java
@@ -6,6 +6,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller 
 public class HomeController {
 	
+	@GetMapping("/restapi")
+	public String restapi() {
+		return "rest-api";
+	}
+	
+	@GetMapping("/js")
+	public String js() {
+		return "js-study";
+	}
+	
 	@GetMapping("/")
 	public String home() {
 		return "home";
@@ -19,5 +29,10 @@ public class HomeController {
 	@GetMapping("/ic")
 	public String ic() {
 		return "id-class";
+	}
+	
+	@GetMapping("/layout")
+	public String layout() {
+		return "layout";
 	}
 }
